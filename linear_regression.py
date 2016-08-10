1af177e00d41311027aeb11919db0ca5e797e958
tobe <tobeg3oogle@gmail.com>
2016-08-10 16:08:14 +0800

    Print more info in linear regression model

--- linear_regression.py
+++ linear_regression.py
@@ -3,25 +3,25 @@
 import tensorflow as tf
 import numpy as np
 
+# Prepare train data
 train_X = np.linspace(-1, 1, 100)
 train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.33 + 10
 
+# Define the model
 X = tf.placeholder("float")
 Y = tf.placeholder("float")
-
 w = tf.Variable(0.0, name="weight")
 b = tf.Variable(0.0, name="bias")
+loss = tf.square(Y - tf.mul(X, w) - b)
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-cost_op = tf.square(Y - tf.mul(X, w) - b)
-
-train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost_op)
-
+# Create session to run
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
-
+    
+    epoch = 1
     for i in range(10):
         for (x, y) in zip(train_X, train_Y):
-            sess.run(train_op, feed_dict={X: x, Y: y})
-
-    print(sess.run(w))
-    print(sess.run(b))
+            _, w_value, b_value = sess.run([train_op, w, b], feed_dict={X: x, Y: y})
+        print("Epoch: {}, w: {}, b: {}".format(epoch, w_value, b_value))
+        epoch += 1
