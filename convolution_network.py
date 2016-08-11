8e5905ae4954176ba799fea9387c434d6c43f989
tobe <tobeg3oogle@gmail.com>
2016-08-11 14:42:35 +0800

    Add print tensorflow version

--- convolution_network.py
+++ convolution_network.py
@@ -10,10 +10,10 @@ trainlabel = mnist.train.labels
 testimg = mnist.test.images
 testlabel = mnist.test.labels
 
-learning_rate = 0.001
-training_epochs = 10
+learning_rate = 0.1
+training_epochs = 1000
 batch_size = 256
-display_step = 1
+display_step = 10
 
 n_input = 784
 n_output = 10
@@ -21,11 +21,9 @@ n_output = 10
 x = tf.placeholder("float", [None, n_input])
 y = tf.placeholder("float", [None, n_output])
 
+# Convolute layer
 w1 = tf.Variable(tf.random_normal([3, 3, 1, 64]))
-w2 = tf.Variable(tf.random_normal([14 * 14 * 64, n_output]))
 b1 = tf.Variable(tf.random_normal([64]))
-b2 = tf.Variable(tf.random_normal([n_output]))
-
 input_r = tf.reshape(x, shape=[-1, 28, 28, 1])
 conv1 = tf.nn.conv2d(input_r, w1, strides=[1, 1, 1, 1], padding="SAME")
 conv2 = tf.nn.bias_add(conv1, b1)
@@ -34,28 +32,27 @@ pool = tf.nn.max_pool(conv3,
                       ksize=[1, 2, 2, 1],
                       strides=[1, 2, 2, 1],
                       padding="SAME")
-dense = tf.reshape(pool, [-1, w2.get_shape().as_list()[0]])
 
+# Full connected layer
+w2 = tf.Variable(tf.random_normal([14 * 14 * 64, n_output]))
+b2 = tf.Variable(tf.random_normal([n_output]))
+dense = tf.reshape(pool, [-1, w2.get_shape().as_list()[0]])
 pred = tf.add(tf.matmul(dense, w2), b2)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
 optm = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
 
-for epoch in range(training_epochs):
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(mnist.train.num_examples / batch_size)
 
-    avg_cost = 0
-    total_batch = int(mnist.train.num_examples / batch_size)
-
-    for i in range(total_batch):
-
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        sess.run(optm, feed_dict={x: batch_xs, y: batch_ys})
-        avg_cost += sess.run(cost, feed_dict={x: batch_xs,
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(optm, feed_dict={x: batch_xs, y: batch_ys})
+            avg_cost += sess.run(cost, feed_dict={x: batch_xs,
                                               y: batch_ys}) / total_batch
 
-    if epoch % display_step == 0:
-        print("Epoch: {}, Cost: {}".format(epoch, avg_cost))
-
-sess.close()
+        if epoch % display_step == 0:
+            print("Epoch: {}, cost: {}".format(epoch, avg_cost))
---
+++ print_tensorflow_version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+
+print(tf.__version__)
