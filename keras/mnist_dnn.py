d717791b00fe5f95b8152a57e558afd1db3e8f9f
tobe <tobeg3oogle@gmail.com>
2018-01-09 17:00:04 +0800

    Not use local dataset in mnist dnn by default

--- keras/mnist_dnn.py
+++ keras/mnist_dnn.py
@@ -18,9 +18,9 @@ def main():
   LOSS_TYPE = "categorical_crossentropy"
 
   # Get training dataset
-  # (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data()
-  (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data(
-      "/Users/tobe/.keras/datasets/mnist.npz")
+  # (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data(
+  #     "/Users/tobe/.keras/datasets/mnist.npz")
+  (X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data()
   X_train = X_train.reshape(60000, 784)
   X_test = X_test.reshape(10000, 784)
   X_train = X_train.astype("float32")
