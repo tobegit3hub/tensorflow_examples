b4998bf0cc8b3ccde7b80929b4d8d7eb735c8383
tobe <tobeg3oogle@gmail.com>
2019-09-18 11:51:08 +0800

    Add linear regression for tensorflow1

---
+++ tensorflow1/linear_regression.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import numpy as np
+import tensorflow as tf
+
+# Prepare train data
+train_X = np.linspace(-1, 1, 100)
+train_Y = 2 * train_X + np.random.randn(*train_X.shape) * 0.33 + 10
+
+# Define the model
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+w = tf.Variable(0.0, name="weight")
+b = tf.Variable(0.0, name="bias")
+loss = tf.square(Y - X * w - b)
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+# Create session to run
+with tf.Session() as sess:
+  sess.run(tf.initialize_all_variables())
+
+  epoch = 1
+  for i in range(10):
+    for (x, y) in zip(train_X, train_Y):
+      _, w_value, b_value = sess.run(
+          [train_op, w, b],
+          feed_dict={X: x,
+                     Y: y})
+
+    print("Epoch: {}, w: {}, b: {}".format(epoch, w_value, b_value))
+    epoch += 1
+
+
